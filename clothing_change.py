1181259ebee3e8be4d4b7bd33df96ed93ed534fa
Gauthamsenthil <gauthamsenthilraja@gmail.com>
2025-04-03 11:02:12 +0530

    submit

---
+++ clothing_change.py
@@ -0,0 +1,5 @@
+temp = int(input("enter the temperature in the celcius form"))
+if temp == 25 or 26:
+    print("you are eligible to wear white clothes")
+else:
+    print("you are not eligible to wear white clothes")
