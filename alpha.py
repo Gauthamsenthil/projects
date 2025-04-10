7a855ec3130562a763b39973e3e3f3fed83c35af
Gauthamsenthil <gauthamsenthilraja@gmail.com>
2025-04-10 20:52:59 +0530

    si

---
+++ alpha.py
@@ -0,0 +1,5 @@
+ans = input("enter any keys to check if it is a string number or special characters")
+if ans == 'a' or'b' or'c' or'd' or'e' or'f' or'g' or'h' or'i' or'j' or'k' or'l' or'm' or'n' or'o' or'p' or'q' or'r' or's' or't' or'u' or'v' or'w' or'x' or'y' or'z' or'A' or'B' or'C' or'D' or'E' or'F' or'G' or'H' or'I' or'J' or'K' or'L' or'M' or'N' or'O' or'P' or'Q' or'R' or'S' or'T' or'U' or'V' or'W' or'X' or'Y' or'Z':
+    print("it is an alphabet")
+else:
+    print("it is not a alphabet")
