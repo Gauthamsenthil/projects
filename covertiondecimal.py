3861bcd80035e0f1505f705f819419617b5c58ca
Gauthamsenthil <gauthamsenthilraja@gmail.com>
2025-05-06 19:58:45 +0530

    submit

---
+++ covertiondecimal.py
@@ -0,0 +1,9 @@
+# Decimal to other systems
+decimal = 42
+
+binary = bin(decimal)      # '0b101010'
+octal = oct(decimal)       # '0o52'
+hexadecimal = hex(decimal) # '0x2a'
+
+print(binary, octal, hexadecimal)
+
