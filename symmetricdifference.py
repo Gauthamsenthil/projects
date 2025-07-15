7ca65acfac9381cbad8caede9020e736d300f655
Gauthamsenthil <gauthamsenthilraja@gmail.com>
2025-07-15 19:46:05 +0530

    submit

---
+++ symmetricdifference.py
@@ -0,0 +1,7 @@
+set1 = {'blue','green'}
+set2 = {'blue','yellow'}
+set3 = {1,2,3,4,5}
+set4 = {1,5,6,7,8,9}
+result1 = set1.symmetric_difference(set2)
+result2 = set3.symmetric_difference(set4)
+print(result1,"\n",result2)
